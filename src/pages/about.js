4c636c308a787b950e4aab68d4d8069d5a69cc42
Darian Singh <53981547+Dariansingh@users.noreply.github.com>
2023-02-22 13:59:28 -0600

    added Jean Bio page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,29 @@
+import arnold from "../images/people/Arnold-Schwarzeneggers-Diet-and-Workout-Plan.jpg"
 export default function About() {
     return (
         <>
-          <h1>About</h1>
+          <div className='bios'>
+            <div className='left-half'>
+            <h1>Jean</h1>
+                <a className='img-container'>
+                    <img className='bios-img' alt="bios-photo" src={arnold}/>
+                </a>
+            </div>
+            <div className='right-half'>
+                <blockquote className='bios-desc' >
+                  <h1>Misson Statement</h1>
+                    <p>
+                        Im the boss
+                    </p>
+                </blockquote>
+                <blockquote className='bios-desc' >
+                  <h1>Bio Thing</h1>
+                    <p>
+                       additional text
+                    </p>
+                </blockquote>
+            </div>
+        </div>
         </>
       );
 }
