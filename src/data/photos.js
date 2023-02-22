c1f7e3bae0344ac2fa435567487553d9fe93dbb5
Darian Singh <53981547+Dariansingh@users.noreply.github.com>
2023-02-22 16:07:31 -0600

    fixed carousel using map function

---
+++ src/data/photos.js
@@ -0,0 +1,34 @@
+export const Photos = [
+    {
+        "id": 0,
+        "imageUrl": require("../images/about/0.jpg"),
+    },
+    {
+        "id": 1,
+        "imageUrl": require("../images/about/1.jpg"),
+    },
+    {
+        "id": 2,
+        "imageUrl": require("../images/about/2.jpg"),
+    },
+    {
+        "id": 3,
+        "imageUrl": require("../images/about/3.jpg"),
+    },
+    {
+        "id": 4,
+        "imageUrl": require("../images/about/4.jpg"),
+    },
+    {
+        "id": 5,
+        "imageUrl": require("../images/about/5.jpg"),
+    },
+    {
+        "id": 6,
+        "imageUrl": require("../images/about/6.jpg"),
+    },
+    {
+        "id": 7,
+        "imageUrl": require("../images/about/6.jpg"),
+    },
+]    
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,29 +1,38 @@
-import arnold from "../images/people/Arnold-Schwarzeneggers-Diet-and-Workout-Plan.jpg"
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Photos } from '../data/photos';
+
 export default function About() {
-    return (
-        <>
-          <div className='bios'>
-            <div className='left-half'>
-            <h1>Jean</h1>
-                <a className='img-container'>
-                    <img className='bios-img' alt="bios-photo" src={arnold}/>
-                </a>
-            </div>
-            <div className='right-half'>
-                <blockquote className='bios-desc' >
-                  <h1>Misson Statement</h1>
-                    <p>
-                        Im the boss
-                    </p>
-                </blockquote>
-                <blockquote className='bios-desc' >
-                  <h1>Bio Thing</h1>
-                    <p>
-                       additional text
-                    </p>
-                </blockquote>
-            </div>
-        </div>
-        </>
-      );
+  return (
+    <div className='bios'>
+      <div className='left-half'>
+        <h1>Jean</h1>
+        <Carousel interval={2250}>
+          {Photos.map(photo => (
+            <Carousel.Item key={photo.id}>
+              <img
+                className='bios-img'
+                alt='bios-photo'
+                src={photo.imageUrl}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+      <div className='right-half'>
+        <blockquote className='bios-desc'>
+          <h1>Misson Statement</h1>
+          <p>Im the boss</p>
+        </blockquote>
+        <blockquote className='bios-desc'>
+          <h1>Bio Thing</h1>
+          <p>additional text</p>
+        </blockquote>
+      </div>
+    </div>
+  );
 }
+
+{/* <a className='img-container'>
+<img className='bios-img' alt="bios-photo" src={arnold}/>
+</a> */}
