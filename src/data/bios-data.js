b7a32dfd6267e6674d330b79f369c7d1512310c5
MrBurstein <33134268+MrBurstein@users.noreply.github.com>
2023-02-22 07:52:45 -0600

    updated navbar and bios

---
+++ src/data/bios-data.js
@@ -0,0 +1,82 @@
+export const Data = [
+    {
+        "id": 0,
+        "firstname": "James",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker that loves to lift. I also am the governor of california and have starred in movies",
+        "imageUrl": require("../images/people/Arnold-Schwarzeneggers-Diet-and-Workout-Plan.jpg"),
+        "secondImageUrl": "fdasd"
+    },
+    {
+        "id": 1,
+        "firstname": "Jeremy",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "../images/people/Arnold-Schwarzeneggers-Diet-and-Workout-Plan.jpg",
+        "secondImageUrl": "fdasd"
+    },
+    {
+        "id": 2,
+        "firstname": "Zach",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "asddasd",
+        "secondImageUrl": "fdasd"
+    },
+    {
+        "id": 3,
+        "firstname": "Danny",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "asddasd",
+        "secondImageUrl": "fdasd"
+    },
+    {
+        "id": 4,
+        "firstname": "Jarelle",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "asddasd",
+        "secondImageUrl": "fdasd"
+    } ,
+    {
+        "id": 5,
+        "firstname": "Rachelle",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "../images/people/Arnold-Schwarzeneggers-Diet-and-Workout-Plan.jpg",
+        "secondImageUrl": "fdasd"
+    },
+    {
+        "id": 6,
+        "firstname": "Bill",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "asddasd",
+        "secondImageUrl": "fdasd"
+    },
+    {
+        "id": 7,
+        "firstname": "Jerm",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "asddasd",
+        "secondImageUrl": "fdasd"
+    },
+    {
+        "id": 8,
+        "firstname": "Shaina",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "asddasd",
+        "secondImageUrl": "fdasd"
+    },
+    {
+        "id": 9,
+        "firstname": "Darrell",
+        "lastname": "Brown",
+        "description": "I'm a Social Worker",
+        "imageUrl": "asddasd",
+        "secondImageUrl": "fdasd"
+    }
+]
--- src/pages/bios.js
+++ src/pages/bios.js
@@ -1,17 +1,27 @@
 
-import Data from '../data/bios-data.json'
-import React, { useState} from 'react';
+import {Data} from '../data/bios-data'
+import { useParams } from "react-router-dom";
+import "../style/bios.css";
 
-export default function Bios(x) {
 
-    const [data, setData] = useState(null);
-
-    setData(Data[x]);
+export default function Bios() {
+    let { id } = useParams(); 
 
     return(
-        <div>
-            <h1>{data.id}</h1>
-            <h3> { data.firstname} </h3>
+        <div className='bios'>
+            <div className='left-half'>
+                <h1> { Data[id].firstname + " " + Data[id].lastname }  </h1>
+                <a className='img-container'>
+                    <img className='bios-img' alt="bios-photo" src={Data[id].imageUrl}/>
+                </a>
+            </div>
+            <div className='right-half'>
+                <blockquote className='bios-desc' >
+                    <p>
+                        {Data[id].description}
+                    </p>
+                </blockquote>
+            </div>
         </div>
     )
 }
