90fe090647daf6c278ea871902baafd2e51291c0
Darian Singh <53981547+Dariansingh@users.noreply.github.com>
2023-02-24 15:07:00 -0600

    commit

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("sidebar-button").addEventListener("click", function(){
-    document.getElementById("sidebar").classList.toggle("open-sidebar");
-});
-
-document.getElementById("dropdown-button").addEventListener("click", function(){
-    document.getElementById("dropdown").classList.toggle("open-dropdown");
-});
